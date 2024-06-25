89d4596f77dbda5f6c12f0986445b299f6e7b3e4
Cecchy2 <dariocecchinato@gmail.com>
2024-06-25 15:00:14 +0200

    installati bootstrap e react bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.scss';
+import MyNav from "./components/MyNav";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNav/>
+    </div>
+  );
+}
+
+export default App;
