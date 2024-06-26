83fd25b99670ca46b6e8a4ecf0faaeb4692d7dcd
Cecchy2 <dariocecchinato@gmail.com>
2024-06-26 09:05:28 +0200

    creati bottoni per scegliere genere libri

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,27 +1,94 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import fantasy from "./data/fantasy.json";
+import history from "./data/history.json";
+import horror from "./data/horror.json";
+import romance from "./data/romance.json";
+import scifi from "./data/scifi.json";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 class AllTheBooks extends Component {
+  state = {
+    books: [], // Holds the books of the selected genre
+    genre: "", // Holds the current genre
+  };
+
+  // Method to load books based on genre
+  loadBooks = (genre) => {
+    switch (genre) {
+      case "fantasy":
+        this.setState({ books: fantasy, genre });
+        break;
+      case "history":
+        this.setState({ books: history, genre });
+        break;
+      case "horror":
+        this.setState({ books: horror, genre });
+        break;
+      case "romance":
+        this.setState({ books: romance, genre });
+        break;
+      case "scifi":
+        this.setState({ books: scifi, genre });
+        break;
+      default:
+        this.setState({ books: [], genre: "" }); // Clear books if no genre matches
+    }
+  };
+
   render() {
+    const { books, genre } = this.state;
     return (
       <Container>
         <Row>
-          {fantasy.map((book, index) => (
+          <Col>
+            <Button variant="secondary" onClick={() => this.loadBooks("fantasy")}>
+              Fantasy
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" onClick={() => this.loadBooks("history")}>
+              History
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" onClick={() => this.loadBooks("horror")}>
+              Horror
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" onClick={() => this.loadBooks("romance")}>
+              Romance
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="secondary" onClick={() => this.loadBooks("scifi")}>
+              Scifi
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          {books.length > 0 ? (
+            books.map((book, index) => (
+              <Col key={index}>
+                <Card style={{ width: "18rem", marginBottom: "1rem" }}>
+                  <Card.Img variant="top" src={book.img} />
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Text>{book.category}</Card.Text>
+                    <Card.Text>{book.price}€</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          ) : (
             <Col>
-              <Card key={index} style={{ width: "18rem", marginBottom: "1rem" }}>
-                <Card.Img variant="top" src={book.img} />
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <Card.Text>{book.category}</Card.Text>
-                  <Card.Text>{book.price}&euro;</Card.Text>
-                </Card.Body>
-              </Card>
+              <p>Please select a genre to display books.</p>
             </Col>
-          ))}
+          )}
         </Row>
       </Container>
     );
